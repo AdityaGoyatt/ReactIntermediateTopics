f2fbfa3797eb9bd2432ba025489820754ab595aa
Aditya <aditya.goyat023@gmail.com>
2023-06-25 15:51:56 -0700

    Zustand Implemented in UserLogin

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,7 +1,13 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
+
   return (
     <>
       <h1>Oops...</h1>
+      {isRouteErrorResponse(error) ? "InvalidPage" : "UnepectedError"}
       <p>Sorry, an unexpected error has occurred.</p>
     </>
   );
